b4d9b15264d2333e1304423d4e643412a4d8eb93
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-09-13 08:54:07 +0530

    Add routing middleware ---this is final step to convert  Console app to WebApi app

--- src/Asp.NetCoreFromScratch/Asp.NetCoreFromScratch/Startup.cs
+++ src/Asp.NetCoreFromScratch/Asp.NetCoreFromScratch/Startup.cs
@@ -31,6 +31,15 @@ using Microsoft.Extensions.Logging;
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hi Web Api project");
+                });
+                   
+            });
         }
     }
