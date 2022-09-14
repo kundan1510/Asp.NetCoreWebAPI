b70a57005350efbbdecf9e071b2cf82a0e207568
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-09-14 23:03:36 +0530

    Type Route Constraints:

---
+++ src/Asp.NetCoreFromScratch/Asp.NetCoreFromScratch/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreFromScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(10):max(100)}")]
+        public string GetById(int id)
+        {
+            return "hello int " + id;
+        }
+        [Route("{id:length(5):alpha}")]
+        public string GetByIdString(string id)
+        {
+            return "hello string " + id;
+        }
+    }
+}
