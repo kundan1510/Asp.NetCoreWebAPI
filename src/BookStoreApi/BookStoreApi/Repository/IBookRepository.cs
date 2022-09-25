2d40a414b8f5c745fd2c666fecbe10e919422e4c
Kundan Kumar <kundan.kumar01@nagarro.com>
2022-09-25 12:44:03 +0530

    Created BookStoreApi and added BookRepository

---
+++ src/BookStoreApi/BookStoreApi/Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStoreApi.Repository
+{
+    public interface IBookRepository
+    {
+        //Task<List<BookModel>> GetAllBooksAsync();
+        //Task<BookModel> GetBookByIdAsync(int bookId);
+        //Task<int> AddBookAsync(BookModel bookModel);
+        //Task UpdateBookAsync(int bookId, BookModel bookModel);
+        //Task UpdateBookPatchAsync(int bookId, JsonPatchDocument bookModel);
+        //Task DeleteBookAsync(int bookId);
+    }
+}
